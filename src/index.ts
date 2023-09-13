7294126549c5b979f0fff4c153203b0131d35c36
Jéssica Sobreira <117686537+jessica-sobreira@users.noreply.github.com>
2023-09-13 00:13:40 -0300

    in

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// Crie uma função que receba 2 números e retorne a média.
+
+
+let num1:number = 3
+let num2:number = 2
+
+function media (num1:number, num2:number) {
+let mediaNumeros = (num1+num2)/2
+return mediaNumeros
+}
+
+console.log(media(num1,num2));
